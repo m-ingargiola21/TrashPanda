34e97aecd9f892b2ccc26095f9c952669045cdc2
thomas-mahon <thomas.mahon@loop.colum.edu>
2017-04-10 18:11:11 -0500

    Fix inventory screen to layout group, start dialogu

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour {
+    [SerializeField]
+    string initialDialogueToDisplay;
+    [SerializeField]
+    string[][] questionDialogueAndAnswerOptions;
+    [SerializeField]
+    GameObject dialoguePanel;
+	
+    void OnCollisionEnter(Collision coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            dialoguePanel.SetActive(true);
+            dialoguePanel.GetComponentInChildren<Text>().text = initialDialogueToDisplay;
+        }
+    }
+
+}
