0791ea132cd3d60b9a8cfc3f3fac2d64ee129026
thomas-mahon <thomas.mahon@loop.colum.edu>
2017-04-11 19:28:07 -0500

    DialogueTrigger

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class DialogueTrigger : MonoBehaviour {
     [SerializeField]
@@ -9,7 +10,22 @@ public class DialogueTrigger : MonoBehaviour {
     string[][] questionDialogueAndAnswerOptions;
     [SerializeField]
     GameObject dialoguePanel;
-	
+    [SerializeField]
+    bool[] changesRelationshipStat;
+    [SerializeField]
+    RelationshipManager relationshipManager;
+    
+    [SerializeField]
+    string [] answerString;
+
+    private void Awake() {
+        if (changesRelationshipStat.Length != answerString.Length)
+        {
+            throw new Exception("Answers must address all of the components individually matching the elements indexies.");
+        }
+        relationshipManager.AddDialogueAnswerListener(gameObject);
+    }
+
     void OnCollisionEnter(Collision coll)
     {
         if(coll.gameObject.tag == "Player")
@@ -17,6 +33,11 @@ public class DialogueTrigger : MonoBehaviour {
             dialoguePanel.SetActive(true);
             dialoguePanel.GetComponentInChildren<Text>().text = initialDialogueToDisplay;
         }
+    }
+
+    public void Activate() {
+
+        return;
     }
 
 }
