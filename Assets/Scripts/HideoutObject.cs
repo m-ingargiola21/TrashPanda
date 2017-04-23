806fb3282c45a911c3a7092db6111ad73d9a017b
Michael Ingargiola <michael.ingargiola@loop.colum.edu>
2017-04-22 19:36:54 -0500

    Adds Hideout 1.0
    Adds:
    hideout scene, altered camera, one floor tile, one test hideout object,
    hideout object movement script(basic Up/Down/Left/Right)
    
    TODO:
    Finish whitebox, add 3ds view, add inventory, add overlap detection

---
+++ Assets/Scripts/HideoutObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HideoutObject : MonoBehaviour
+//M.I.
+{
+    [SerializeField] string movementVerticalAxisName;
+    [SerializeField] string movementHorizontalAxisName;
+    [SerializeField] float hideoutObjectMovementX;
+    [SerializeField] float hideoutObjectMovementZ;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        HideoutObjectMovementX();
+	}
+
+    private void HideoutObjectMovementX()
+    {
+        if (Input.GetButtonDown(movementVerticalAxisName))
+            if (Input.GetAxisRaw(movementVerticalAxisName) > 0) //moves hideout object forward
+                transform.Translate(0, 0, -hideoutObjectMovementZ);
+            else if(Input.GetAxisRaw(movementVerticalAxisName) < 0) //moves hideout object backward
+                transform.Translate(0, 0, hideoutObjectMovementZ);
+
+        if (Input.GetButtonDown(movementHorizontalAxisName))
+            if (Input.GetAxisRaw(movementHorizontalAxisName) > 0) //moves hideout object left
+                transform.Translate(-hideoutObjectMovementX, 0, 0);
+            else if (Input.GetAxisRaw(movementHorizontalAxisName) < 0) //moves hideout object right
+                transform.Translate(hideoutObjectMovementX, 0, 0);
+
+
+        //Current position of hideout object and prints postition to console
+        Vector3 hideoutObjectCurrentPosition = transform.position;
+        print(hideoutObjectCurrentPosition);
+
+    }
+}
