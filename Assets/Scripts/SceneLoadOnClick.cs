6755d1570c792fb2d08ba1584ae58623c2a15016
thomas-mahon <thomas.mahon@loop.colum.edu>
2017-04-25 13:03:00 -0500

    Fix main menu

---
+++ Assets/Scripts/SceneLoadOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadOnClick : MonoBehaviour {
+
+    [SerializeField]
+    string sceneToLoad;
+
+    public void LoadScene() {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
